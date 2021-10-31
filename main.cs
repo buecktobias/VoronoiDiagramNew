a71ba550abd146c7cbd7612a7e144e6217537312
Tobi 16.10 <44665917+buecktobias@users.noreply.github.com>
2021-10-31 21:37:17 +0000

    Point Creator

--- main.cs
+++ main.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+ 
 
 class Point2D{
   private int _x;
@@ -25,7 +26,16 @@ class Point2D{
 }
 
 class VoronoiSite : Point2D {
-  public VoronoiSite(int X, int Y) : base(X, Y) { }
+  private char _siteLetter;
+  private char _cellLetter;
+  public VoronoiSite(int X, int Y, char siteLetter, char cellLetter) : base(X, Y) { 
+    this._siteLetter = siteLetter;
+    this._cellLetter = cellLetter;
+  }
+
+  public override string ToString(){
+    return base.ToString() + " - " + this._siteLetter;
+  }
 
 }
 
@@ -71,22 +81,72 @@ class NearestVoronoiSiteFinder{
   }
 }
 
+class Rectangle{
+  private Point2D _bottomLeftPoint;
+  private Point2D _topRightPoint;
+
+  public Rectangle(Point2D BottomLeftPoint, Point2D TopRightPoint){
+    this._bottomLeftPoint = BottomLeftPoint;
+    this._topRightPoint = TopRightPoint;
+  }
+
+
+  public Point2D getBottomLeftPoint(){
+    return this._bottomLeftPoint;
+  }
+
+  public Point2D getTopRightPoint(){
+    return this._topRightPoint;
+  }
+  
+}
+
+class PointCreator{
+
+  public static List<List<Point2D>> createAllPointsInsideRectangle(Rectangle rectangle){
+
+    List<List<Point2D>> points = new List<List<Point2D>>();
+    for(int x = rectangle.getBottomLeftPoint().GetX(); x <= rectangle.getTopRightPoint().GetX(); x++){
+      List<Point2D> column = new List<Point2D>();
+      for(int y = rectangle.getBottomLeftPoint().GetY(); y <= rectangle.getTopRightPoint().GetY();y++){
+          column.Add(new Point2D(x, y));
+      }
+      points.Add(column);
+
+    }
+    return points;
+  }
+
+}
+
+class VoronoiDiagramCreator{
+
+}
+ 
+
 
 class Program {
   public static void Main (string[] args) {
 
-    VoronoiSite vs1 = new VoronoiSite(2,2);
-    VoronoiSite vs2 = new VoronoiSite(2,8);
-    VoronoiSite vs3 = new VoronoiSite(3,5);
+    VoronoiSite vs1 = new VoronoiSite(2, 2, 'A', 'a');
+    VoronoiSite vs2 = new VoronoiSite(2, 8, 'B', 'b');
+    VoronoiSite vs3 = new VoronoiSite(3, 5, 'C', 'c');
     List<VoronoiSite> allVoronoiSites = new List<VoronoiSite>();
     allVoronoiSites.Add(vs1);
     allVoronoiSites.Add(vs2);
     allVoronoiSites.Add(vs3);
-
+    Console.WriteLine(allVoronoiSites);
     var point1 = new Point2D(3, 5);
     NearestVoronoiSiteFinder nearestVoronoiSiteFinder = new NearestVoronoiSiteFinder(new EuclideanDistanceCalculator());
 
     Console.WriteLine(nearestVoronoiSiteFinder.findNearestVoronoiSite(point1, allVoronoiSites));
-    
+    List<List<Point2D>> points = PointCreator.createAllPointsInsideRectangle(new Rectangle(new Point2D(0,0), new Point2D(3,3)));
+    foreach(var column in points){
+      foreach(var y in column){
+        Console.WriteLine(y);
+      }
+    }
+
+
   }
 }

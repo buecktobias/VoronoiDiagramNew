687a97afe148293ca22f33cb2b575872d7d9a774
Tobi 16.10 <44665917+buecktobias@users.noreply.github.com>
2021-10-31 17:45:48 +0000

    New Class Point2D

--- main.cs
+++ main.cs
@@ -1,7 +1,32 @@
 using System;
 
+class Point2D{
+  private int _x;
+  private int _y;
+
+  public Point2D(int X, int Y){
+    this._x = X;
+    this._y = Y;
+  }
+
+  public int GetX(){
+    return this._x;
+  }
+
+  public int GetY(){
+    return this._y;
+  }
+
+  public override string ToString(){
+    return "[" + this.GetX() + ", " + this.GetY() + "]";
+  }
+
+}
+
+
 class Program {
   public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
+    var point = new Point2D(10, 5);
+    Console.WriteLine(point);
   }
 }

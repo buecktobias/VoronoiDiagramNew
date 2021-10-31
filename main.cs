344aa96e046fa2345dcfc3c06943bba805d8a078
Tobi 16.10 <44665917+buecktobias@users.noreply.github.com>
2021-10-31 19:09:01 +0000

    created NearestVoronoiSiteFInder, which finds the nearest Voronoi Site for a specific point

--- main.cs
+++ main.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 class Point2D{
   private int _x;
@@ -23,9 +24,18 @@ class Point2D{
 
 }
 
+class VoronoiSite : Point2D {
+  public VoronoiSite(int X, int Y) : base(X, Y) { }
 
-class EuclideanDistanceCalculator{
-  public static double calculateDistance(Point2D point1, Point2D point2){
+}
+
+
+interface IDistanceCalculator{
+  public double calculateDistance(Point2D point1, Point2D point2);
+}
+
+class EuclideanDistanceCalculator : IDistanceCalculator{
+  public double calculateDistance(Point2D point1, Point2D point2){
       int XDiff = Math.Abs(point1.GetX() - point2.GetX());
       int YDiff = Math.Abs(point1.GetY() - point2.GetY());
       int XDiffSquared = XDiff * XDiff;
@@ -34,11 +44,49 @@ class EuclideanDistanceCalculator{
   }
 }
 
+class NearestVoronoiSiteFinder{
+  private IDistanceCalculator _distanceCalculator;
+
+  public NearestVoronoiSiteFinder(IDistanceCalculator DistanceCalculator){
+      this._distanceCalculator = DistanceCalculator;
+  }
+
+  public VoronoiSite findNearestVoronoiSite(Point2D fromPoint, List<VoronoiSite> allVoronoiSites){
+    if(allVoronoiSites.Count == 0){
+      throw new Exception("voronoi Sites length must be at least one");
+    }else{
+      VoronoiSite NearestVoronoiSiteSoFar = allVoronoiSites[0];
+      double DistanceNearestVoronoiSiteSoFar = 999_999_999;
+      foreach(var voronoiSite in allVoronoiSites){
+          double distanceToVoronoiSite = this._distanceCalculator.calculateDistance(fromPoint, voronoiSite);
+
+          if(distanceToVoronoiSite < DistanceNearestVoronoiSiteSoFar){
+            NearestVoronoiSiteSoFar = voronoiSite;
+            DistanceNearestVoronoiSiteSoFar = distanceToVoronoiSite;
+          }
+
+      }
+      return NearestVoronoiSiteSoFar;
+    }
+  }
+}
+
 
 class Program {
   public static void Main (string[] args) {
-    var point1 = new Point2D(10, 5);
-    var point2 = new Point2D(0, 0);
-    Console.WriteLine(EuclideanDistanceCalculator.calculateDistance(point1, point2));
+
+    VoronoiSite vs1 = new VoronoiSite(2,2);
+    VoronoiSite vs2 = new VoronoiSite(2,8);
+    VoronoiSite vs3 = new VoronoiSite(3,5);
+    List<VoronoiSite> allVoronoiSites = new List<VoronoiSite>();
+    allVoronoiSites.Add(vs1);
+    allVoronoiSites.Add(vs2);
+    allVoronoiSites.Add(vs3);
+
+    var point1 = new Point2D(3, 5);
+    NearestVoronoiSiteFinder nearestVoronoiSiteFinder = new NearestVoronoiSiteFinder(new EuclideanDistanceCalculator());
+
+    Console.WriteLine(nearestVoronoiSiteFinder.findNearestVoronoiSite(point1, allVoronoiSites));
+    
   }
 }
